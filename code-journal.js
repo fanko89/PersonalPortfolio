26df3286f5a7f10edeffc136fdd9429e4ba859ec
Derek Ney <deek311@gmail.com>
2021-03-02 11:17:31 -0700

    starwars

--- code-journal.js
+++ code-journal.js
@@ -82,5 +82,5 @@ const myCar = {
    // a higher order function is a function that accepts another function as a parameter.
 // filter, map and reduce are the most popular, but forEach, every, find, and some are also HOFs
 
-const theFunction = () => { //multiple lines use curly braces and 'return' keyword
+const theFunction = () => { //multiple lines use curly braces and 'return' keyword 
   return 'I am awesome'
