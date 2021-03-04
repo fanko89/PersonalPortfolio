db802fe7cc122bc13d9b5c2056c8d6dee58eae83
Derek Ney <deek311@gmail.com>
2021-03-04 10:59:26 -0700

    starwars update

---
+++ characters/main.js
@@ -0,0 +1,28 @@
+import { people } from '../data/people.js'
+
+const mainElement = document.querySelector('#main')
+
+people.forEach((person) => {
+    const charFigure = document.createElement('figure')
+    const charImg = document.createElement('img')
+    let charNum = getLastNumber(person.url)
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+    const charCaption = document.createElement('figcaption')
+    charCaption.textContent = person.name
+
+    charFigure.appendChild(charImg)
+    charFigure.appendChild(charCaption)
+
+    mainElement.appendChild(charFigure)
+})
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
+
+const maleCharacters = people.filter(person)
