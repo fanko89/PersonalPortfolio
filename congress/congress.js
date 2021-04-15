4a39e7200fa28dd8b4a7836b2107b79d8a848d5f
Derek Ney <deek311@gmail.com>
2021-04-15 11:01:05 -0600

    added to pokie

--- congress/congress.js
+++ congress/congress.js
@@ -1,9 +1,13 @@
+  
 import { senators } from '../data/senators.js'
 import { representatives } from '../data/representatives.js'
+import { removeChildren } from '../utils/index.js'
 
 const congressGrid = document.querySelector('.congressGrid')
 const seniorityButton = document.querySelector('#seniorityButton')
 const birthdayButton = document.querySelector('#birthdayButton')
+const missedVotesButton = document.querySelector('#missedVotes')
+const partyHackButton = document.querySelector('#partyHack')
 
 seniorityButton.addEventListener('click', () => {
     senioritySort()
@@ -13,8 +17,17 @@ birthdayButton.addEventListener('click', () => {
     birthdaySort()
 })
 
+missedVotesButton.addEventListener('click', () => {
+    alert(`${missedVotesRep.name} missed votes ${missedVotesRep.missed_votes_pct}% of the time!`)
+})
+
+partyHackButton.addEventListener('click', () => {
+    console.log(partyHack)
+    //alert(partyHack)
+})
+
 function populateCongressGrid(simplePeople) {
-    console.log(simplePeople)
+    removeChildren(congressGrid)
     simplePeople.forEach(person => {
         let personDiv = document.createElement('div')
         personDiv.className = 'figureDiv'
@@ -37,10 +50,31 @@ function getSimplifiedCongress(congressPeople) {
         let middleName = person.middle_name ? `${person.middle_name}` : ``
         return {
             id: person.id,
+            title: person.title,
             name: `${person.first_name} ${middleName} ${person.last_name}`,
-            imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-100px.jpeg`
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-100px.jpeg`,
+            seniority: parseInt(person.seniority, 10),
+            date_of_birth: parseInt(person.date_of_birth, 10),
+            missed_votes_pct: person.missed_votes_pct,
+            votes_with_party_pct: person.votes_with_party_pct
         }
     })
 }
+
+function senioritySort() {
+     populateCongressGrid(getSimplifiedCongress(senators).sort(
+        (a, b) => a.seniority - b.seniority
+    ).reverse())
+}
+
+function birthdaySort() {
+    populateCongressGrid(getSimplifiedCongress(senators).sort(
+        (a, b) => a.date_of_birth - b.date_of_birth
+    ))
+}
+
+const missedVotesRep = getSimplifiedCongress(representatives).filter((rep) => rep.title === 'Representative').reduce((acc, rep) => acc.missed_votes_pct > rep.missed_votes_pct ? acc : rep)
+
+const partyHack = getSimplifiedCongress(representatives).filter((rep) => rep.title === 'Representative').reduce((acc, rep) => acc.votes_with_party_pct > rep.votes_with_party_pct ? acc : rep)
 
 populateCongressGrid(getSimplifiedCongress(senators))
--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,8 +1,36 @@
 const pokeGrid = document.querySelector('.pokeGrid')
 const loadButton = document.querySelector('.loadPokemon')
+const fetchButton = document.querySelector('#fetchSelectedPokemon')
+
+const dialog = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+const modalBackground = document.querySelector('.modal-background')
+const submitButton = document.querySelector('#submitButton')
+
+closeButton.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+
+modalBackground.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
 
 loadButton.addEventListener('click', () => {
     loadPage()
+})
+
+fetchButton.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+    /* getAPIData(`https://pokeapi.co/api/v2/pokemon/25`).then(
+        (data) => {
+            populatePokeCard(data)
+        }
+    ) */
+})
+
+submitButton.addEventListener('click', () => {
+    let inputField = document.querySelector('.input')
+    inputValue = inputField.value
 })
 
 async function getAPIData(url) {
@@ -17,9 +45,58 @@ async function getAPIData(url) {
 }
 
 function loadPage() {
-    getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=150`).then(
-        (data) => {
-            console.log(data)
+    getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25&offset=748`).then(
+        async (data) => {
+            for (const singlePokemon of data.results) {
+                await getAPIData(singlePokemon.url).then(
+                    (pokeData) => populatePokeCard(pokeData)
+                )
+            }
         }
     )
 }
+
+function populatePokeCard(singlePokemon) {
+   // console.log(singlePokemon)
+    let pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    let pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    pokeCard.addEventListener('click', () => {
+        pokeCard.classList.toggle('is-flipped')
+    })
+    pokeCard.appendChild(populateCardFront(singlePokemon))
+    pokeCard.appendChild(populateCardBack(singlePokemon))
+    pokeScene.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeScene)
+}
+
+function populateCardFront(pokemon) {
+    console.log(pokemon)
+    let pokeFront = document.createElement('div')
+    pokeFront.className = 'card__face card__face--front'
+    let frontLabel = document.createElement('p')
+    frontLabel.textContent = pokemon.name
+    let frontImage = document.createElement('img')
+    frontImage.src = getImageFileName(pokemon)
+
+    pokeFront.appendChild(frontLabel)
+    pokeFront.appendChild(frontImage)
+    return pokeFront
+}
+
+function populateCardBack(pokemon) {
+    let pokeBack = document.createElement('div')
+    pokeBack.className = 'card__face card__face--back'
+    let backLabel = document.createElement('p')
+    backLabel.textContent = `Moves: ${pokemon.moves.length}`
+    pokeBack.appendChild(backLabel)
+    return pokeBack
+}
+
+function getImageFileName(pokemon) {
+    let pokeId
+    if (pokemon.id < 10) pokeId = `00${pokemon.id}`
+    if (pokemon.id > 9 && pokemon.id < 100) pokeId = `0${pokemon.id}`
+    if (pokemon.id > 99 && pokemon.id < 810) pokeId = pokemon.id
+    return `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${pokeId}.png`
